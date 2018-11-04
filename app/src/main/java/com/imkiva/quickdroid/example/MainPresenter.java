42e86c3859206fd35f5decc6aabc3e8f4c71a901
imkiva <libkernelpanic@gmail.com>
2018-11-05 00:03:09 +0800

    project: upload to bintray

--- app/src/main/java/com/imkiva/quickdroid/example/MainPresenter.java
+++ app/src/main/java/com/imkiva/quickdroid/example/MainPresenter.java
@@ -1,5 +1,6 @@
 package com.imkiva.quickdroid.example;
 
+import com.imkiva.quickdroid.QuickDatabase;
 import com.imkiva.quickdroid.persistence.QPersistOnce;
 import com.imkiva.quickdroid.persistence.QPreference;
 import com.imkiva.quickdroid.ui.presenter.Presenter;
@@ -20,7 +21,11 @@ public class MainPresenter extends Presenter<MainActivity> {
 
     void loadWelcome() {
         QPersistOnce.of("first_used")
-                .run(() -> QPreference.of("main").setPreference("first", false));
+                .run(() -> {
+                    QPreference.of("main").setPreference("first", false);
+                    QuickDatabase.open("main").dropAllTables();
+                });
+
         postIfAvailable(MainActivity::onConfigureLoaded);
     }
 }
