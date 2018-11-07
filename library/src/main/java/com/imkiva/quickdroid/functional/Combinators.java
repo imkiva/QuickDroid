67e6f44535d336f1e21933b51bef810caba23f53
imkiva <libkernelpanic@gmail.com>
2018-11-07 23:34:24 +0800

    improve: change to interface

--- library/src/main/java/com/imkiva/quickdroid/functional/Combinators.java
+++ library/src/main/java/com/imkiva/quickdroid/functional/Combinators.java
@@ -3,8 +3,8 @@ package com.imkiva.quickdroid.functional;
 /**
  * @author kiva
  */
-public final class Combinators {
-    public static <T, R> QFunction<T, R> Y(QFunction<QFunction<T, R>, QFunction<T, R>> ff) {
+public interface Combinators {
+    static <T, R> QFunction<T, R> Y(QFunction<QFunction<T, R>, QFunction<T, R>> ff) {
         return ff.apply(f -> Y(ff).apply(f));
     }
 }
